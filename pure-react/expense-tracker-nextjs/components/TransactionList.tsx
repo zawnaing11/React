32e57d49909e74dc6c602f6b0e3f280a0835bef9
zawnaingtun-mbridge <zaw.naing.htun@frobom.com>
2024-08-16 16:33:05 +0630

    expense-tracker-nextjs

---
+++ pure-react/expense-tracker-nextjs/components/TransactionList.tsx
@@ -0,0 +1,23 @@
+import { Transaction } from "@/types/Transaction";
+import getTransaction from "@/app/actions/getTransaction";
+import TransactionItem from "./TransactionItem";
+
+const TransactionList = async () => {
+    const { transactions, error } = await getTransaction();
+    if (error) {
+        return <p className="error">{ error }</p>
+    }
+
+    return ( <>
+        { transactions && transactions.length > 0 ? <h3>History</h3> : '' }
+        <ul className="list">
+            {
+                transactions && transactions.map((transaction: Transaction) => (
+                    <TransactionItem key={transaction.id} transaction={transaction} />
+                ))
+            }
+        </ul>
+    </> );
+}
+
+export default TransactionList;
